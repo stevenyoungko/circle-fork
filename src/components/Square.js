64e983ae2df822b946e3164466e518c205e6a8e4
steven <steven_ko@paradise-soft.com.tw>
2022-04-07 16:28:55 +0800

    pref: 把Square component 改成 functional component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,9 @@
-import React from 'react';
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={ () => this.props.onClick() }
-      >
-        { this.props.value }
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square
