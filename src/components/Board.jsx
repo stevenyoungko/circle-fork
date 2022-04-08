247c6feaf4d1c7f047086bbfb12d2b6ca4282020
steven <steven_ko@paradise-soft.com.tw>
2022-04-08 17:18:08 +0800

    pref(Board): 格子用for迴圈跑出來

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,7 +4,8 @@ import Square from './Square'
 function Board(props) {
   function renderSquare(i) {
     return( 
-      <Square 
+      <Square
+        key={i}
         value={props.squares[i]}
         onClick={() => props.onClick(i)} 
       />
@@ -13,21 +14,17 @@ function Board(props) {
 
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {
+        Array.from({ length: 3}).map((_, y) => (
+          <div className='board-row' key={y}>
+            {
+              Array.from({ length: 3 }).map((_, x) => (
+                renderSquare(y*3 + x)
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   )
 }
